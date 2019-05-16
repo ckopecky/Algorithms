ba7d14ac7aa615d6adb44650b80dc483399ed6ce
ckopecky <kopecky12112@gmail.com>
2019-05-16 11:22:06 -0700

    Solved with no regard to time throughout the day
    TODO: Solve with regard to time of day.
    
    Index in list indicates later in day.

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,8 +2,35 @@
 
 import argparse
 
+# Right now this takes into account buying at min at selling at max regardless of time. Did not realize that the list was indicative of time throughout the day. 
+
 def find_max_profit(prices):
-  pass
+  current_min_price = float("inf")
+  current_max_price = -float("inf")
+
+  if len(prices) == 0:
+    profit = None
+    return profit
+  
+  if len(prices) == 1:
+    current_min_price = prices[0]
+    current_max_price = prices[0]
+    profit = current_max_price - current_min_price
+    return profit
+
+  else:
+    i = 0
+    while i < len(prices):
+      if prices[i] < current_min_price:
+        current_min_price = prices[i]
+      if prices[i] > current_max_price:
+        current_max_price = prices[i]
+      i = i + 1
+    profit = current_max_price - current_min_price
+    return profit
+    
+  
+
 
 
 if __name__ == '__main__':
