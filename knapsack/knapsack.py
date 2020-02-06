0b110e3e4d8dba5831775add2f54d1b43d253db3
ckopecky <kopecky12112@gmail.com>
2020-02-06 10:17:45 -0800

    making change and plan for knapsack

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -5,9 +5,18 @@ from collections import namedtuple
 
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
-def knapsack_solver(items, capacity):
-  pass
-  
+
+"""
+Brute-force knapsack checks every possible combination of items we could 
+be taking and outputs the combination with the best value. 
+1. Use recursion to exhaustively check every single combination of items
+2. Base case 1: we have no items left in the pile to take
+3. Base case 2: we have one item left in the pile. Check to see if it fits. If it does, take it, otherwise discard it.
+4. On each recursive call we pick up the next item from the pile
+5. Calculate the overall value we have in our knapsack if we don't take the item
+6. Calculate the overall value we have in our knapsack if we do take the item
+7. Compare the two calculated values; take the option that yields the greater value
+"""
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
